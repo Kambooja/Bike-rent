74f506cee375b6878075f9ba08bb781ed43e2aaa
Maniaks13 <pawel.bilan@student.wat.edu.pl>
2022-06-15 20:17:24 +0200

    Spring initialize

---
+++ src/main/java/com/sha/serverbikemanagement/ServerBikeManagementApplication.java
@@ -0,0 +1,15 @@
+package com.sha.serverbikemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBikeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBikeManagementApplication.class, args);
+	}
+	{
+	}
+
+}
---
+++ src/test/java/com/sha/serverbikemanagement/ServerBikeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.serverbikemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBikeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
