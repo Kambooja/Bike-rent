38d78b96015dc986c749ff83285c1b55c5a565c8
Maniaks13 <pawel.bilan@student.wat.edu.pl>
2022-06-15 21:55:36 +0200

    Add to DB table Bike

---
+++ src/main/java/com/sha/serverbikemanagement/model/Bike.java
@@ -0,0 +1,24 @@
+package com.sha.serverbikemanagement.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "bike")
+public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "description")
+    private Double description;
+}
