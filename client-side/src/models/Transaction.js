bcbaa21e5af5d545133924aafcf771bc43e680c0
Maniaks13 <pawel.bilan@student.wat.edu.pl>
2022-06-17 09:49:34 +0200

    Client-SIDE: Add model classes

---
+++ client-side/src/models/Transaction.js
@@ -0,0 +1,8 @@
+export class Transaction{
+    constructor(user,bike,transactionDate,id) {
+        this.user=user;
+        this.bike=bike;
+        this.transactionDate=transactionDate;
+        this.id=id;
+    }
+}
